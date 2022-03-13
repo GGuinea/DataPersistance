77765bf93907c466cc9790fcd7fab1ab5b5272a4
Gguinea <Gguinea>
2022-03-13 08:43:11 +0100

    Initial

---
+++ backend/src/test/java/proj/platformy/dane/ApplicationTests.java
@@ -0,0 +1,13 @@
+package proj.platformy.dane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
